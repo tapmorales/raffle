343df08d1dafe5e277bbfb3e8afbd9d0a9f9beed
Daniel Tapias Morales <tapmorales@gmail.com>
2020-01-07 22:39:25 +0000

    fix bug

--- src/lib/sort.js
+++ src/lib/sort.js
@@ -1,5 +1,5 @@
 module.exports.sort = (min = 0, max = 10, integer = true) => {
-  let r = Math.random() * (max - min + 1) + min + 1;
+  let r = Math.random() * (max - min + 1) + min;
 
   return integer ? parseInt(r) : r;
 };
